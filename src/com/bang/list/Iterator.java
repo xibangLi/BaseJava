103670a795817e71b9bcdf22841a08be47c2ae66
xibangLi <413287840@qq.com>
2018-01-11 23:08:15 +0800

    ff

--- src/com/bang/list/Iterator.java
+++ src/com/bang/list/Iterator.java
@@ -2,7 +2,9 @@ package com.bang.list;
 
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 /**
  * Created by bang on 18-1-11.
@@ -12,8 +14,17 @@ public class Iterator {
 
 
     public static void main(String[] args) {
+        //Map -> AbstractMap ->HashMap
         Map<String,Object> map = new HashMap<>();
         map.put("name","lixibang");
+
+        //Map -> AbstractMap -> HashMap -> LinkedHashMap
+        Map<String, Object> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("name","jiangmeng");
+
+        //Map -> AbstractMap ->
+        Map<String,Object> weakHashMap = new WeakHashMap<>();
+        weakHashMap.put("name","li si");
 
 
 
