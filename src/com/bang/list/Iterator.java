999f7e88954a888dbdd02996bed2c53e55c3262c
xibangLi <413287840@qq.com>
2018-01-11 22:13:37 +0800

    java基础

---
+++ src/com/bang/list/Iterator.java
@@ -0,0 +1,22 @@
+package com.bang.list;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by bang on 18-1-11.
+ */
+public class Iterator {
+
+
+
+    public static void main(String[] args) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","lixibang");
+
+
+
+    }
+
+}
